cbe2c36c6076676710c65e3cb712e6ec5ab0e465
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-30 17:23:19 +0530

    Jenkins IP Update

---
+++ Jenkins-ip-update/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "vijayavani-terraform-state"
+    key = "misc/jenkins-ip-update/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_instance" "jenkins" {
+  instance_id = "i-01ba56d4506f07930"
+}
+
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins"
+  type    = "A"
+  zone_id = "Z01843807JDTGXSQ26S6"
+  ttl = 10
+  records = [data.aws_instance.jenkins.public_ip]
+}
